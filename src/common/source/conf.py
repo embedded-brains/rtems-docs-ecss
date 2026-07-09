d1a102a87b72fc38cb17a8a0343ac1eb736e0874
Sebastian Huber <sebastian.huber@embedded-brains.de>
2026-07-10 02:18:12 +0800

    docs(common): Enable numfig
    Signed-off-by: Sebastian Huber <sebastian.huber@embedded-brains.de>

--- src/common/source/conf.py
+++ src/common/source/conf.py
@@ -89,6 +89,11 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+# Number figures, tables, and code-blocks continuously across the whole
+# document instead of restarting per chapter (the LaTeX book/report class
+# default), so that duplicate "Fig. 1" captions cannot occur across chapters.
+numfig = True
+
 
 # -- Options for HTML output ----------------------------------------------
 
